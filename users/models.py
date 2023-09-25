cdbfcfb8a914cc22ff4acdfbb1303246c259f386
Egor Volkov <volkovqa96@gmail.com>
2023-09-25 20:55:11 +0300

    Описана модель Пользователя

--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.db import models
+
+
+NULLABLE = {
+    'null': True,
+    'blank': True
+}
 
 
 class User(AbstractUser):
-    pass
+    """Модель пользователя"""
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    telegram = models.CharField(unique=True, verbose_name='telegram', **NULLABLE)
+    chat_id = models.PositiveIntegerField(default=0, verbose_name='Номер чата', **NULLABLE)
+    update_id = models.PositiveIntegerField(default=0, verbose_name='Номер последнего сообщения', **NULLABLE)
+    name = models.CharField(max_length=50, verbose_name="Имя", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
